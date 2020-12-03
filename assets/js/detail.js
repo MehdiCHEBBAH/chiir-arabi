28185a8e31dc46bff0afc97611c5b9d7a4f95e9d
Mehdi CHEBBAH <gm_chebbah@esi.dz>
2020-12-03 16:48:41 +0100

    fixed a bug

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,20 +1,27 @@
 
 (function($) {
-    $.getJSON("../../data.json", function(data){
-        var id = +location.search.substring(1).split("=")[1];
-        var element = data.find(e => e.id == id);
-        document.title = element.title;
-        $("#chiir_image").attr("src",`images/${element.id}.png`);
-        $('#chiir_title').html(element.title);
-        $('#chiir_author').html(element.author? `قال ${element.author}`: '');
-        $('#chiir_content').html(element.content.split("\\n").join("<br />"));
-        for(let e of element.tags){
-            $('#chiir_tags').append(`
-                <a class="tag after second" href="index.html?q=${encodeURIComponent(e.substring(1).replaceAll("_", " "))}">${e} </a>
-            `);
+
+    $.ajax({
+        url: "http://mehdi-chebbah.ml/chiir-arabi/data.json",
+        dataType: 'json',
+        async: false,
+        success: function(loaded_data) {
+            var id = +location.search.substring(1).split("=")[1];
+            var element = data.find(e => e.id == id);
+            document.title = element.title;
+            $("#chiir_image").attr("src",`images/${element.id}.png`);
+            $('#chiir_title').html(element.title);
+            $('#chiir_author').html(element.author? `قال ${element.author}`: '');
+            $('#chiir_content').html(element.content.split("\\n").join("<br />"));
+            for(let e of element.tags){
+                $('#chiir_tags').append(`
+                    <a class="tag after second" href="index.html?q=${encodeURIComponent(e.substring(1).replaceAll("_", " "))}">${e} </a>
+                `);
+            }
+        },
+        fail: function(err){
+            console.log("An error has occurred.");
         }
-    }).fail(function(){
-        console.log("An error has occurred.");
-    });
+      });
     
 })(jQuery);
